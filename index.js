8edc0e1d77a60485aa4f075bc71d57c1a12bf423
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-25 07:23:53 +0600

    cors modified

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 require('dotenv').config()
-
-// app.use(cors())
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors())
+// const corsConfig = {
+//     origin: '',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }
+// app.use(cors(corsConfig))
+// app.options("", cors(corsConfig))
 app.use(express.json());
 
 app.get('/', (req, res) => {
